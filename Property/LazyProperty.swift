ae5cb27449bdda3ccceddad106c408ef646c50da
Mallikharjuna-001 <arjun2281993@gmail.com>
2025-09-15 13:14:10 +0530

    Lazy Example

--- Property/LazyProperty.swift
+++ Property/LazyProperty.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+//A stored property that is created only when first accessed.
+//Useful for expensive initialization. Must be declared with var.
+class DataLoader {
+    lazy var fileData : String = {
+        print("Loading file...")
+        return "Big file Content"
+    }()
+}
