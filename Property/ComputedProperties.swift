f5aa105b39f4ef1507012222b3f43aac4265ab54
Mallikharjuna-001 <arjun2281993@gmail.com>
2025-09-15 13:11:37 +0530

    Heading

--- Property/ComputedProperties.swift
+++ Property/ComputedProperties.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// Computed Property
 
 struct Rectangle {
     
---
+++ Property/LazyProperty.swift
@@ -0,0 +1,8 @@
+//
+//  Lazy Property.swift
+//  Property
+//
+//  Created by Mallikharjuna on 15/09/25.
+//
+
+import Foundation
