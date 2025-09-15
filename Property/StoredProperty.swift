611510d416c46366e0c71ae7a8bc16dfd2fc8346
Mallikharjuna-001 <arjun2281993@gmail.com>
2025-09-15 11:10:02 +0530

    Class can be Stored property

--- Property/StoredProperty.swift
+++ Property/StoredProperty.swift
@@ -9,9 +9,31 @@ import Foundation
 import UIKit
 
 // Stored Property
+
+//Actually store values in memory.
+//Can be variable (var) or constant (let).
+//Only available in classes and structs (not in enums by default).
+
+
+
 struct StoredProperty{
     
     var name:String
     var dob:String
     var age:Int
+}
+class StoredPropertyClass{
+    var name:String
+    var age:Int
+    var profile:String
+    var dob:String
+    var gender:String
+    
+    init(name:String,age:Int,profile:String,dob:String,gender:String){
+        self.name = name
+        self.age = age
+        self.profile = profile
+        self.dob = dob
+        self.gender = gender
+    }
 }
